7de70ea7089be067764a0fd39b57482a03b56e92
Andret2344 <andret2344@gmail.com>
2020-02-08 00:18:38 +0100

    Fixes, fixes, FIXES
    Signed-off-by: Andret2344 <andret2344@gmail.com>

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,16 +2,14 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Queue queue = new Queue();
-        Queue queue1 = new Queue();
-        Person person1 = new Person("Mateusz");
-        Person person2 = new Person("Andrzej");
-        Person person3 = new Person("Marcin");
-        Person person4 = new Person("Krystian");
-        queue.add(person1);
-        queue1.add(person1);
-        System.out.println(queue1.equals(queue));
-
-    }
+	public static void main(String[] args) {
+		Queue queue = new Queue();
+		Queue queue1 = new Queue();
+		Person person1 = new Person("Mateusz");
+		queue.add(person1);
+		queue1.add(person1);
+		System.out.println(queue1.equals(queue));
+		queue1.clear();
+		System.out.println(queue1.size());
+	}
 }
--- src/test/java/com/company/QueueTest.java
+++ src/test/java/com/company/QueueTest.java
@@ -3,115 +3,117 @@ package com.company;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class QueueTest {
-    @Test
-    public void getCorrectPersonAfterAdd() {
-        // given
-        Queue queue = new Queue();
-        Person person = new Person("Maciej");
-        queue.add(person);
-        // when
-        Person gotPerson = queue.get();
-        // then
-        assertEquals(person, gotPerson);
-    }
+	@Test
+	public void getCorrectPersonAfterAdd() {
+		// given
+		Queue queue = new Queue();
+		Person person = new Person("Maciej");
+		queue.add(person);
+		// when
+		Person gotPerson = queue.get();
+		// then
+		assertEquals(person, gotPerson);
+	}
 
-    @Test
-    public void containsNullAfterAddingNull() {
-        // given
-        Queue queue = new Queue();
-        queue.add(null);
-        // when
-        boolean result = queue.contains(null);
-        // then
-        assertTrue(result);
-    }
+	@Test
+	public void containsNullAfterAddingNull() {
+		// given
+		Queue queue = new Queue();
+		queue.add(null);
+		// when
+		boolean result = queue.contains(null);
+		// then
+		assertTrue(result);
+	}
 
-    @Test
-    public void containsPersonAfterAddingNull() {
-        // given
-        Person person = new Person("Andrzej");
-        Queue queue = new Queue();
-        queue.add(null);
-        // when
-        boolean result = queue.contains(person);
-        // then
-        assertFalse(result);
-    }
+	@Test
+	public void containsPersonAfterAddingNull() {
+		// given
+		Person person = new Person("Andrzej");
+		Queue queue = new Queue();
+		queue.add(null);
+		// when
+		boolean result = queue.contains(person);
+		// then
+		assertFalse(result);
+	}
 
-    @Test
-    public void containsPersonAfterAddPerson() {
-        // given
-        Queue queue = new Queue();
-        Person person = new Person("Marek");
-        queue.add(person);
-        // when
-        boolean result = queue.contains(person);
-        // then
-        assertTrue(result);
-    }
+	@Test
+	public void containsPersonAfterAddPerson() {
+		// given
+		Queue queue = new Queue();
+		Person person = new Person("Marek");
+		queue.add(person);
+		// when
+		boolean result = queue.contains(person);
+		// then
+		assertTrue(result);
+	}
 
-    @Test
-    public void containsNullAfterAddPerson() {
-        // given
-        Queue queue = new Queue();
-        Person person = new Person("Dariusz");
-        queue.add(person);
-        // when
-        boolean result = queue.contains(null);
-        // then
-        assertFalse(result);
-    }
+	@Test
+	public void containsNullAfterAddPerson() {
+		// given
+		Queue queue = new Queue();
+		Person person = new Person("Dariusz");
+		queue.add(person);
+		// when
+		boolean result = queue.contains(null);
+		// then
+		assertFalse(result);
+	}
 
-    @Test
-    public void getSizeOfEmptyQueue() {
-        // given
-        Queue queue = new Queue();
-        // when
-        int result = queue.size();
-        // then
-        assertEquals(0, result);
-    }
+	@Test
+	public void getSizeOfEmptyQueue() {
+		// given
+		Queue queue = new Queue();
+		// when
+		int result = queue.size();
+		// then
+		assertEquals(0, result);
+	}
 
-    @Test
-    public void getSizeOfQueueWithNull() {
-        // given
-        Queue queue = new Queue();
-        queue.add(null);
-        // when
-        int result = queue.size();
-        // then
-        assertEquals(1, result);
-    }
+	@Test
+	public void getSizeOfQueueWithNull() {
+		// given
+		Queue queue = new Queue();
+		queue.add(null);
+		// when
+		int result = queue.size();
+		// then
+		assertEquals(1, result);
+	}
 
-    @Test
-    public void getSizeOfQueueWithFiveObjects() {
-        // given
-        Queue queue = new Queue();
-        queue.add(new Person("Mateusz"));
-        queue.add(new Person("Dawid"));
-        queue.add(new Person("Dariusz"));
-        queue.add(new Person("Mariusz"));
-        queue.add(new Person("Marek"));
-        // when
-        int result = queue.size();
-        // then
-        assertEquals(5, result);
-    }
+	@Test
+	public void getSizeOfQueueWithFiveObjects() {
+		// given
+		Queue queue = new Queue();
+		queue.add(new Person("Mateusz"));
+		queue.add(new Person("Dawid"));
+		queue.add(new Person("Dariusz"));
+		queue.add(new Person("Mariusz"));
+		queue.add(new Person("Marek"));
+		// when
+		int result = queue.size();
+		// then
+		assertEquals(5, result);
+	}
 
-    @Test
-    public void equalMethodTest() {
-        // given
-        Queue queue1 = new Queue();
-        Queue queue2 = new Queue();
-        Person person = new Person("Dawid");
-        queue1.add(person);
-        queue2.add(person);
-        // when
-        boolean result = queue1.equals(queue2);
-        // then
-        assertTrue(result);
-    }
+	@Test
+	public void equalMethodTest() {
+		// given
+		Queue queue1 = new Queue();
+		Queue queue2 = new Queue();
+		Person person = new Person("Dawid");
+		queue1.add(person);
+		queue2.add(person);
+		// when
+		boolean result = queue1.equals(queue2);
+		// then
+		assertTrue(result);
+	}
 }
