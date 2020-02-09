63ab32f5a57b3483859a72aece50bbacaeb7f7a5
MADDYxMM <dm.mateusz@gmail.com>
2020-02-09 19:02:28 +0100

    Fix formatting

--- src/main/java/com/company/Queue.java
+++ src/main/java/com/company/Queue.java
@@ -5,128 +5,127 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Queue {
-    private Node last;
+	private Node last;
 
-    @Getter
-    @Setter
-    @EqualsAndHashCode
-    private static class Node {
-        private Node prev;
-        private Person person;
+	@Getter
+	@Setter
+	@EqualsAndHashCode
+	private static class Node {
+		private Node prev;
+		private Person person;
 
-        public Node(Person person) {
-            this.person = person;
-        }
+		public Node(Person person) {
+			this.person = person;
+		}
 
-        @Override
-        public String toString() {
-            return String.valueOf(person);
-        }
-    }
+		@Override
+		public String toString() {
+			return String.valueOf(person);
+		}
+	}
 
-    public Person get() {
-        Node walking = last;
-        if (walking.getPrev() == null) {
-            Person flag = walking.getPerson();
-            walking.setPerson(null);
-            return flag;
-        }
-        while (walking.getPrev().getPrev() != null) {
-            walking = walking.getPrev();
-        }
-        Person flag = walking.getPrev().getPerson();
-        walking.setPrev(null);
-        return flag;
-    }
+	public Person get() {
+		Node walking = last;
+		if (walking.getPrev() == null) {
+			Person flag = walking.getPerson();
+			walking.setPerson(null);
+			return flag;
+		}
+		while (walking.getPrev().getPrev() != null) {
+			walking = walking.getPrev();
+		}
+		Person flag = walking.getPrev().getPerson();
+		walking.setPrev(null);
+		return flag;
+	}
 
-    public void add(Person person) {
-        Node node = new Node(person);
-        node.setPrev(last);
-        last = node;
-    }
+	public void add(Person person) {
+		Node node = new Node(person);
+		node.setPrev(last);
+		last = node;
+	}
 
-    public int size() {
-        Node walking = last;
-        if (walking == null) {
-            return 0;
-        }
-        int counter = 0;
-        do {
-            walking = walking.getPrev();
-            counter++;
-        } while (walking != null);
-        return counter;
-    }
+	public int size() {
+		Node walking = last;
+		if (walking == null) {
+			return 0;
+		}
+		int counter = 0;
+		do {
+			walking = walking.getPrev();
+			counter++;
+		} while (walking != null);
+		return counter;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Queue queue = (Queue) o;
-        if (size() != queue.size()) {
-            return false;
-        }
-        Node walking = last;
-        Node walking2 = queue.last;
-        while (walking != null) {
-            if (!walking.equals(walking2)) {
-                return false;
-            }
-            walking = walking.getPrev();
-            walking2 = walking2.getPrev();
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Queue queue = (Queue) o;
+		if (size() != queue.size()) {
+			return false;
+		}
+		Node walking = last;
+		Node walking2 = queue.last;
+		while (walking != null) {
+			if (!walking.equals(walking2)) {
+				return false;
+			}
+			walking = walking.getPrev();
+			walking2 = walking2.getPrev();
+		}
+		return true;
+	}
 
+	@Override
+	public int hashCode() {
+		Node walking = last;
+		int prime = 3;
+		int result = 1;
+		while (walking != null) {
+			result = prime * result + (walking.hashCode());
+			walking = walking.getPrev();
+		}
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-        Node walking = last;
-        int prime = 3;
-        int result = 1;
-        while (walking != null) {
-            result = prime * result + (walking.hashCode());
-            walking = walking.getPrev();
-        }
-        return result;
-    }
+	public boolean contains(Person person) {
+		Node walking = last;
+		if (size() == 0) {
+			return false;
+		}
+		do {
+			if (walking.getPerson() == person) {
+				return true;
+			}
+			if (walking.getPerson() == null) {
+				return false;
+			}
+			if (walking.getPerson().equals(person)) {
+				return true;
+			}
+			walking = walking.getPrev();
+		} while (walking != null);
+		return false;
+	}
 
+	public void clear() {
+		last = null;
+	}
 
-    public boolean contains(Person person) {
-        Node walking = last;
-        if (size() == 0) {
-            return false;
-        }
-        do {
-            if (walking.getPerson() == person) {
-                return true;
-            }
-            if (walking.getPerson() == null) {
-                return false;
-            }
-            if (walking.getPerson().equals(person)) {
-                return true;
-            }
-            walking = walking.getPrev();
-        } while (walking != null);
-        return false;
-    }
-
-    public void clear() {
-        last = null;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        do {
-            s.append(last.getPerson());
-            last = last.prev;
-        } while (last != null);
-        return s.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		Node walking = last;
+		while (walking != null) {
+			s.append(walking);
+			walking = walking.prev;
+		}
+		return s.toString();
+	}
 }
--- src/test/java/com/company/QueueTest.java
+++ src/test/java/com/company/QueueTest.java
@@ -1,302 +1,349 @@
 package com.company;
 
-
 import org.junit.Test;
 
 import java.util.HashSet;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class QueueTest {
-    @Test
-    public void getCorrectPersonAfterAdd() {
-        // given
-        Queue queue = new Queue();
-        Person person = new Person("Maciej");
-        queue.add(person);
-        // when
-        Person gotPerson = queue.get();
-        // then
-        assertEquals(person, gotPerson);
-    }
+	@Test
+	public void getCorrectPersonAfterAdd() {
+		// given
+		Queue queue = new Queue();
+		Person person = new Person("Maciej");
+		queue.add(person);
 
-    @Test
-    public void getCorrectPersonAfterAddFewObjects() {
-        // given
-        Queue queue = new Queue();
-        Person person1 = new Person("Maciek");
-        Person person2 = new Person("Mateusz");
-        Person person3 = new Person("Dawid");
-        Person person4 = new Person("Pawel");
-        queue.add(person1);
-        queue.add(person2);
-        queue.add(person3);
-        queue.add(person4);
-        // when
-        Person gotPerson = queue.get();
-        Person gotPerson2 = queue.get();
-        Person gotPerson3 = queue.get();
-        Person gotPerson4 = queue.get();
-        // then
-        assertEquals(person1, gotPerson);
-        assertEquals(person2, gotPerson2);
-        assertEquals(person3, gotPerson3);
-        assertEquals(person4, gotPerson4);
-    }
+		// when
+		Person gotPerson = queue.get();
 
-    @Test
-    public void containsEmptyQueue() {
-        // given
-        Queue queue = new Queue();
-        Person person = new Person("Damian");
-        // when
-        boolean result = queue.contains(person);
-        // then
-        assertFalse(result);
-    }
+		// then
+		assertEquals(person, gotPerson);
+	}
 
-    @Test
-    public void containsNullAfterAddingNull() {
-        // given
-        Queue queue = new Queue();
-        queue.add(null);
-        // when
-        boolean result = queue.contains(null);
-        // then
-        assertTrue(result);
-    }
+	@Test
+	public void getCorrectPersonAfterAddFewObjects() {
+		// given
+		Queue queue = new Queue();
+		Person person1 = new Person("Maciek");
+		Person person2 = new Person("Mateusz");
+		Person person3 = new Person("Dawid");
+		Person person4 = new Person("Pawel");
+		queue.add(person1);
+		queue.add(person2);
+		queue.add(person3);
+		queue.add(person4);
 
-    @Test
-    public void containsPersonAfterAddingNull() {
-        // given
-        Person person = new Person("Andrzej");
-        Queue queue = new Queue();
-        queue.add(null);
-        // when
-        boolean result = queue.contains(person);
-        // then
-        assertFalse(result);
-    }
+		// when
+		Person gotPerson = queue.get();
+		Person gotPerson2 = queue.get();
+		Person gotPerson3 = queue.get();
+		Person gotPerson4 = queue.get();
 
-    @Test
-    public void containsPersonAfterAddPerson() {
-        // given
-        Queue queue = new Queue();
-        Person person = new Person("Marek");
-        queue.add(person);
-        // when
-        boolean result = queue.contains(person);
-        // then
-        assertTrue(result);
-    }
+		// then
+		assertEquals(person1, gotPerson);
+		assertEquals(person2, gotPerson2);
+		assertEquals(person3, gotPerson3);
+		assertEquals(person4, gotPerson4);
+	}
 
-    @Test
-    public void containsNullAfterAddPerson() {
-        // given
-        Queue queue = new Queue();
-        Person person = new Person("Dariusz");
-        queue.add(person);
-        // when
-        boolean result = queue.contains(null);
-        // then
-        assertFalse(result);
-    }
+	@Test
+	public void containsEmptyQueue() {
+		// given
+		Queue queue = new Queue();
+		Person person = new Person("Damian");
 
-    @Test
-    public void containsPersonWithTheSameName() {
-        // given
-        Queue queue = new Queue();
-        Queue queue2 = new Queue();
-        Person person = new Person("Maciej");
-        Person person2 = new Person("Maciej");
-        queue.add(person);
-        queue2.add(person2);
-        // when
-        boolean result = queue.contains(person2);
-        // then
-        assertTrue(result);
-    }
+		// when
+		boolean result = queue.contains(person);
 
-    @Test
-    public void getSizeOfEmptyQueue() {
-        // given
-        Queue queue = new Queue();
-        // when
-        int result = queue.size();
-        // then
-        assertEquals(0, result);
-    }
+		// then
+		assertFalse(result);
+	}
 
-    @Test
-    public void getSizeOfQueueWithNull() {
-        // given
-        Queue queue = new Queue();
-        queue.add(null);
-        // when
-        int result = queue.size();
-        // then
-        assertEquals(1, result);
-    }
+	@Test
+	public void containsNullAfterAddingNull() {
+		// given
+		Queue queue = new Queue();
+		queue.add(null);
 
-    @Test
-    public void getSizeOfQueueWithFiveObjects() {
-        // given
-        Queue queue = new Queue();
-        queue.add(new Person("Mateusz"));
-        queue.add(new Person("Dawid"));
-        queue.add(new Person("Dariusz"));
-        queue.add(new Person("Mariusz"));
-        queue.add(new Person("Marek"));
-        // when
-        int result = queue.size();
-        // then
-        assertEquals(5, result);
-    }
+		// when
+		boolean result = queue.contains(null);
 
-    @Test
-    public void equalMethodWithSameObjects() {
-        // given
-        Queue queue1 = new Queue();
-        Queue queue2 = new Queue();
-        Person person = new Person("Dawid");
-        queue1.add(person);
-        queue2.add(person);
-        // when
-        boolean result = queue1.equals(queue2);
-        // then
-        assertTrue(result);
-    }
+		// then
+		assertTrue(result);
+	}
 
-    @Test
-    public void equalMethodWithDifferentObjects() {
-        // given
-        Queue queue = new Queue();
-        Queue queue1 = new Queue();
-        Person person1 = new Person("Maciek");
-        Person person2 = new Person("Darek");
-        Person person3 = new Person("Mariusz");
-        queue.add(null);
-        queue.add(person1);
-        queue1.add(null);
-        queue1.add(person2);
-        queue.add(person3);
-        queue1.add(person3);
-        // when
-        boolean result = queue.equals(queue1);
-        // then
-        assertFalse(result);
-    }
+	@Test
+	public void containsPersonAfterAddingNull() {
+		// given
+		Person person = new Person("Andrzej");
+		Queue queue = new Queue();
+		queue.add(null);
 
-    @Test
-    public void equalMethodWithDifferentSize() {
-        // given
-        Queue queue = new Queue();
-        Queue queue1 = new Queue();
-        Person person1 = new Person("Maciek");
-        Person person2 = new Person("Dariusz");
-        queue.add(person1);
-        queue.add(person2);
-        queue1.add(person1);
-        // when
-        boolean result = queue.equals(queue1);
-        // then
-        assertFalse(result);
-    }
+		// when
+		boolean result = queue.contains(person);
 
-    @Test
-    public void equalThis() {
-        // given
-        Queue queue = new Queue();
-        // when
-        boolean result = queue.equals(queue);
-        //then
-        assertTrue(result);
-    }
+		// then
+		assertFalse(result);
+	}
 
-    @Test
-    public void equalsNull() {
-        // given
-        Queue queue = new Queue();
-        // when
-        boolean result = queue.equals(null);
-        // then
-        assertFalse(result);
-    }
+	@Test
+	public void containsPersonAfterAddPerson() {
+		// given
+		Queue queue = new Queue();
+		Person person = new Person("Marek");
+		queue.add(person);
 
-    @Test
-    public void clearTest() {
-        // given
-        Queue queue = new Queue();
-        queue.add(new Person("Maciej"));
-        queue.add(new Person("Dariusz"));
-        queue.add(new Person("Arkadiusz"));
-        // when
-        queue.clear();
-        // then
-        assertEquals(0, queue.size());
-    }
+		// when
+		boolean result = queue.contains(person);
 
-    @Test
-    public void clearWithNull() {
-        // given
-        Queue queue = new Queue();
-        queue.add(null);
-        queue.add(null);
-        queue.add(null);
-        // when
-        queue.clear();
-        // then
-        assertEquals(0, queue.size());
-    }
+		// then
+		assertTrue(result);
+	}
 
-    @Test
-    public void clearEmptyQueue() {
-        // given
-        Queue queue = new Queue();
-        // when
-        queue.clear();
-        // then
-        assertEquals(0, queue.size());
-    }
+	@Test
+	public void containsNullAfterAddPerson() {
+		// given
+		Queue queue = new Queue();
+		Person person = new Person("Dariusz");
+		queue.add(person);
 
-    @Test
-    public void hashCodeTest() {
-        // given
-        HashSet<Queue> hashSet = new HashSet<>();
-        Queue queue = new Queue();
-        Queue queue1 = new Queue();
-        Queue queue2 = new Queue();
-        queue.add(new Person("Maciej"));
-        queue1.add(new Person("Dariusz"));
-        queue2.add(new Person("Pawel"));
-        hashSet.add(queue);
-        hashSet.add(queue1);
-        hashSet.add(queue2);
-        // when
-        int result = hashSet.size();
-        // then
-        assertEquals(3, result);
-    }
+		// when
+		boolean result = queue.contains(null);
 
-    @Test
-    public void toStringTest() {
-        // given
-        Queue queue = new Queue();
-        queue.add(new Person("Mateusz"));
-        // when
-        String result = queue.toString();
-        // then
-        assertEquals("Person(name=Mateusz)", result);
-    }
+		// then
+		assertFalse(result);
+	}
 
-//    @Test
-//    public void toStringNode() {
-//        // given
-//        Person person = new Person("Mateusz");
-//        Queue.Node node = new Queue.Node(person);
-//        // when
-//        String result = node.toString();
-//        // then
-//        assertEquals("Person(name=Mateusz)", result);
-//    }
+	@Test
+	public void containsPersonWithTheSameName() {
+		// given
+		Queue queue = new Queue();
+		Queue queue2 = new Queue();
+		Person person = new Person("Maciej");
+		Person person2 = new Person("Maciej");
+		queue.add(person);
+		queue2.add(person2);
+
+		// when
+		boolean result = queue.contains(person2);
+
+		// then
+		assertTrue(result);
+	}
+
+	@Test
+	public void getSizeOfEmptyQueue() {
+		// given
+		Queue queue = new Queue();
+
+		// when
+		int result = queue.size();
+
+		// then
+		assertEquals(0, result);
+	}
+
+	@Test
+	public void getSizeOfQueueWithNull() {
+		// given
+		Queue queue = new Queue();
+		queue.add(null);
+
+		// when
+		int result = queue.size();
+
+		// then
+		assertEquals(1, result);
+	}
+
+	@Test
+	public void getSizeOfQueueWithFiveObjects() {
+		// given
+		Queue queue = new Queue();
+		queue.add(new Person("Mateusz"));
+		queue.add(new Person("Dawid"));
+		queue.add(new Person("Dariusz"));
+		queue.add(new Person("Mariusz"));
+		queue.add(new Person("Marek"));
+
+		// when
+		int result = queue.size();
+
+		// then
+		assertEquals(5, result);
+	}
+
+	@Test
+	public void equalMethodWithSameObjects() {
+		// given
+		Queue queue1 = new Queue();
+		Queue queue2 = new Queue();
+		Person person = new Person("Dawid");
+		queue1.add(person);
+		queue2.add(person);
+
+		// when
+		boolean result = queue1.equals(queue2);
+
+		// then
+		assertTrue(result);
+	}
+
+	@Test
+	public void equalMethodWithDifferentObjects() {
+		// given
+		Queue queue = new Queue();
+		Queue queue1 = new Queue();
+		Person person1 = new Person("Maciek");
+		Person person2 = new Person("Darek");
+		Person person3 = new Person("Mariusz");
+		queue.add(null);
+		queue.add(person1);
+		queue1.add(null);
+		queue1.add(person2);
+		queue.add(person3);
+		queue1.add(person3);
+
+		// when
+		boolean result = queue.equals(queue1);
+
+		// then
+		assertFalse(result);
+	}
+
+	@Test
+	public void equalMethodWithDifferentSize() {
+		// given
+		Queue queue = new Queue();
+		Queue queue1 = new Queue();
+		Person person1 = new Person("Maciek");
+		Person person2 = new Person("Dariusz");
+		queue.add(person1);
+		queue.add(person2);
+		queue1.add(person1);
+
+		// when
+		boolean result = queue.equals(queue1);
+
+		// then
+		assertFalse(result);
+	}
+
+	@Test
+	public void equalThis() {
+		// given
+		Queue queue = new Queue();
+
+		// when
+		boolean result = queue.equals(queue);
+
+		//then
+		assertTrue(result);
+	}
+
+	@Test
+	public void equalsNull() {
+		// given
+		Queue queue = new Queue();
+
+		// when
+		boolean result = queue.equals(null);
+
+		// then
+		assertFalse(result);
+	}
+
+	@Test
+	public void clearTest() {
+		// given
+		Queue queue = new Queue();
+		queue.add(new Person("Maciej"));
+		queue.add(new Person("Dariusz"));
+		queue.add(new Person("Arkadiusz"));
+
+		// when
+		queue.clear();
+
+		// then
+		assertEquals(0, queue.size());
+	}
+
+	@Test
+	public void clearWithNull() {
+		// given
+		Queue queue = new Queue();
+		queue.add(null);
+		queue.add(null);
+		queue.add(null);
+
+		// when
+		queue.clear();
+
+		// then
+		assertEquals(0, queue.size());
+	}
+
+	@Test
+	public void clearEmptyQueue() {
+		// given
+		Queue queue = new Queue();
+
+		// when
+		queue.clear();
+
+		// then
+		assertEquals(0, queue.size());
+	}
+
+	@Test
+	public void hashCodeTest() {
+		// given
+		HashSet<Queue> hashSet = new HashSet<>();
+		Queue queue = new Queue();
+		Queue queue1 = new Queue();
+		Queue queue2 = new Queue();
+		queue.add(new Person("Maciej"));
+		queue1.add(new Person("Dariusz"));
+		queue2.add(new Person("Pawel"));
+		hashSet.add(queue);
+		hashSet.add(queue1);
+		hashSet.add(queue2);
+
+		// when
+		int result = hashSet.size();
+
+		// then
+		assertEquals(3, result);
+	}
+
+	@Test
+	public void toStringTest() {
+		// given
+		Queue queue = new Queue();
+		queue.add(new Person("Mateusz"));
+
+		// when
+		String result = queue.toString();
+
+		// then
+		assertEquals("Person(name=Mateusz)", result);
+	}
+
+	@Test
+	public void testToStringGetAndSize() {
+		// given
+		Queue queue = new Queue();
+		Person person = new Person("Maciej");
+
+		// when
+		queue.add(person);
+
+		// then
+		assertEquals("Person(name=Maciej)", queue.toString());
+		assertEquals(person, queue.get());
+		assertEquals(1, queue.size());
+	}
 }
