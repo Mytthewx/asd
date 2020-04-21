9c63e8f1b0666ab6c519f210727bc82f0d399ffd
Mytthew <dm.mateusz@gmail.com>
2020-04-21 23:11:47 +0200

    RadixSort done

--- src/main/java/eu/mytthew/Main.java
+++ src/main/java/eu/mytthew/Main.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class Main {
 	public static void main(String[] args) {
 		RadixSort radixSort = new RadixSort();
-		int[] array = {923, 625, 1, 364, 723, 662, 98};
+		int[] array = {625, 1, 364, 723, 662, 98, 10, 11, 2, 1};
 		System.out.println(Arrays.toString(array));
 		radixSort.radixSort(array);
+		System.out.println(Arrays.toString(array));
 	}
 }
--- src/main/java/eu/mytthew/RadixSort.java
+++ src/main/java/eu/mytthew/RadixSort.java
@@ -5,36 +5,27 @@ import java.util.Map;
 import java.util.*;
 
 public class RadixSort {
-	public void sort(int[] array) {
-		radixSort(array);
-		System.out.println("Inner toString: " + Arrays.toString(array));
-	}
-
 	public void radixSort(int[] array) {
-		Map<Integer, List<Integer>> map = new HashMap<>();
-		for (int i = 0; i < 10; i++) {
-			map.put(i, new ArrayList<>());
-		}
-		int[] newArray = Arrays.copyOf(array, array.length);
-		for (int value : newArray) {
-			int lastDigit = getLastDigit(value);
-			for (int j = 0; j < 10; j++) {
-				if (j == lastDigit) {
-					map.get(j).add(value);
-				}
+		int max = findMax(array);
+		int maxDigits = getNumberOfDigits(max);
+		int divide = 1;
+		for (int j = maxDigits; j > 0; j--) {
+			Map<Integer, List<Integer>> map = new HashMap<>();
+			for (int i = 0; i < 10; i++) {
+				map.put(i, new ArrayList<>());
 			}
+			for (int value : array) {
+				int valueAfterReduce = value / divide;
+				int bucket = valueAfterReduce % 10;
+				map.get(bucket).add(value);
+			}
+			divide *= 10;
+			int[] sortedArray = map.values().stream()
+					.flatMap(Collection::stream)
+					.mapToInt(e -> e)
+					.toArray();
+			System.arraycopy(sortedArray, 0, array, 0, array.length);
 		}
-	}
-
-	private int getLastDigit(int number) {
-		return number % 10;
-	}
-
-	private int[] reduceNumbers(int[] array) {
-		for (int i = 0; i < array.length; i++) {
-			array[i] /= 10;
-		}
-		return array;
 	}
 
 	private int findMax(int[] array) {
@@ -54,14 +45,5 @@ public class RadixSort {
 			count++;
 		}
 		return count;
-	}
-
-	private int getSpecificDigitOfNumber(int number, int digit) {
-		String s = String.valueOf(number);
-		if (digit < getNumberOfDigits(number)) {
-			char ch = s.charAt(digit);
-			return ch - '0';
-		}
-		return 0;
 	}
 }
